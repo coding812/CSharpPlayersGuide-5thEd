4d1209854be449ffb73a80ec1370331215563477
coding812 <85715642+coding812@users.noreply.github.com>
2023-04-30 10:46:30 -0400

    Added file

---
+++ TheThingNamer3000/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("What kind of thing are we talking about?");
+
+// Assigns the name of the thing to the variable a
+string a = Console.ReadLine();
+
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+
+// Assigns the description of the thing to the variable b
+string b = Console.ReadLine();
+
+// adds "of Doom" to the description of the thing
+string c = "of Doom";
+
+// adds "3000" to the description of the thing
+string d = "3000";
+
+Console.WriteLine("The " + b + " " + a + " of " + c + " " + d + "!");
