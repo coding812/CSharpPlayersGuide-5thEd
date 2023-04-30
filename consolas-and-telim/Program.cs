beed5dba7c868fedddd216591800b0544be82288
coding812 <85715642+coding812@users.noreply.github.com>
2023-04-30 10:25:13 -0400

    set up files
    //

---
+++ consolas-and-telim/Program.cs
@@ -0,0 +1,8 @@
+﻿string name;
+
+Console.WriteLine("Bread is ready!!");
+Console.WriteLine("Who would like some bread?");
+
+name = Console.ReadLine();
+
+Console.WriteLine("Noted: " + name + "got some bread.");
